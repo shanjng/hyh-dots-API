07eb170f3a48f6cb7a3e511526a8cccb949eeab2
shanjng <shanjng@umich.edu>
2019-07-25 18:27:14 +0200

    added login functionality

--- src/services/auth-services.js
+++ src/services/auth-services.js
@@ -5,6 +5,24 @@ module.exports = class AuthService {
     constructor() {}
 
     login(authUser) {
-        
+        return new Promise((resolve, reject) => {
+            User.prototype.getAll()
+            .then(users => {
+                let matched = users.filter(user => {
+                    return user.email == authUser.email
+                })
+                if (matched.length >= 1) {
+                    resolve(authUser)
+                }
+                else {
+                    reject("User not found / Incorrect email")
+                }
+            }
+            )
+            .catch(err => {
+                console.log(err)
+                reject(err);
+            })
+        })
     }
 }
