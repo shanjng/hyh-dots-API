70cf3dea3ccdc4c3501c166c16d11399761a3230
shanjng <shanjng@umich.edu>
2019-07-25 19:29:51 +0200

    dealing with disconnection from mySql

--- src/data-base/data-base.js
+++ src/data-base/data-base.js
@@ -11,11 +11,30 @@ const config = {
   database: "heroku_7a3ad4ad0a6eb7e"
 };
 
-var connection = mysql.createConnection(config);
+var connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect( function onConnect(err) {   // The server is either down
+        if (err) {                     // or restarting (takes while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
+        console.log('db error', err);
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                        // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+handleDisconnect();
 
-connection.connect(function(err) {
-  if (err) console.log(err);
-  console.log("Database Connected: " + config.host + ":" + config.port);
-});
+// connection.connect(function(err) {
+//   if (err) console.log(err);
+//   console.log("Database Connected: " + config.host + ":" + config.port);
+// });
 
 module.exports = connection;
