745c983c261e37ec355a8bf72aae25582b8cfe07
shanjng <shanjng@umich.edu>
2019-08-05 11:27:11 +0200

    added dependencies to package.json for tiwtter

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use("/trends",trends);
-app.use("/tweets",tweets);
-app.use('/users',users);
-
-
 const logger = (req, res, next) => {
     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
@@ -25,13 +19,8 @@ const logger = (req, res, next) => {
 app.use(logger)
 
 // pulls in express library
-const express = require('express');
 var favicon = require('serve-favicon');
 var path = require('path')
-
-// instance of express made
-const app = express();
-var cors = require('cors');
 
 app.use(cors()); // cross-origin resource sharing
 app.use(express.json());
@@ -39,16 +28,13 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
-const logger = (req,res,next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-}
 
 //Middleware execute:
 app.use(logger)
-
+app.use("/trends",trends);
+app.use("/tweets",tweets);
+app.use('/users',users);
 app.use("/api/auth", require("./src/api/auth-routes")); // imports user router
-app.use("/api/user", require("./src/api/user-routes"))
 
 app.get('/*', function(req, res){
     res.json({err: 'Unsupported GET request'})
