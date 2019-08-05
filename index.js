4a8935f477489d123ecc4fef2abb25d5a28fd71c
Christina Campbell <christinacampbell@Christinas-MacBook-Pro.local>
2019-08-05 12:51:43 +0200

    favicon commented out

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(logger)
 
 // pulls in express library
 // const express = require('express');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var path = require('path')
 
 // instance of express made
@@ -34,7 +34,7 @@ app.use(cors()); // cross-origin resource sharing
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 
 
